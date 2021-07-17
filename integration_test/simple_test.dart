d70cf2311be593322eb3122a278ce5f94c922798
NykoriakArtem <61303342+NykoriakArtem@users.noreply.github.com>
2021-07-17 17:11:24 -0400

    commit

--- integration_test/simple_test.dart
+++ integration_test/simple_test.dart
@@ -1,10 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_driver/flutter_driver.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  
+  group('increment app', () {
+      final plusIcon = find.byIcon(Icons.add);
 
-  testWidgets("failing test example", (WidgetTester tester) async {
-    expect(2 + 2, equals(5));
-  });
+      FlutterDriver driver;
+
+      setUpAll(() async {
+        driver = await FlutterDriver.connect(); 
+      });
+
+      tearDownAll(() async{
+        driver.close();
+      });
+
+      test('click plus', ()async{
+        await driver.tap(plusIcon);
+        await driver.waitFor(find.text('1'));
+      });
+    });
 }
